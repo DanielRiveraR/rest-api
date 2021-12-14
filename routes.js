fc9c2db204236f7930f7eac02990d219d1a157df
DanielRiveraR <dani_wooten@hotmail.com>
2021-12-14 09:17:42 -0800

    Fixed a unexpected behavior inPUT and DELETE routes

--- routes.js
+++ routes.js
@@ -98,53 +98,37 @@ router.get('/users', authenticateUser, asyncHandler(async(req,res) => {
 
   //This route updates a course only if the current user owns it.
   router.put('/courses/:id', authenticateUser, asyncHandler(async(req,res) => {
-    const course = await Course.findByPk(req.params.id);
-    const user = await User.findByPk(req.currentUser.id);
 
-    if (course && user) {
-        if (req.body.description && req.body.title) { 
-            if (req.currentUser.id === req.params.userId) {
-                try {
-                    Course.update({
-                        title: req.body.title,
-                        description: req.body.description,
-                        estimatedTime: req.body.estimatedTime,
-                        materialsNeeded: req.body.materialsNeeded,
-                    }, 
-                    {   where: {id: req.params.id} });
-                    await course.save();
+    try {
+        const course = await Course.findByPk(req.params.id);
+        if (course) {
+                if (course.userId === req.currentUser.id) {
+                    await  course.update(req.body);
                     res.status(204).end();
-                    
-                        
-                        
-                } catch (error) {
-                    if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
-                        const errors = error.errors.map(err => err.message);
-                        res.status(400).json({errors});
-                        } else {
-                            throw error;
-                        }
-                    } 
-
+                }else {
+                        res.status(403).json({message: "You can't update a course that you didn't create."});
+                    }
+                } else {
+                    next();
+                }      
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({errors});
             } else {
-                res.status(403).json({message: "You can't update a course that you didn't create."});
-            }
-    } else {
-        res.status(400).json({message: "Please enter a title and a description."});
-    } 
-} else {
-    res.status(404).json({message: "The course that you are trying to update does not exist."});
-}
+                res.status(404).json({message: "The course that you are trying to update does not exist."});
+                throw error;
+            } 
+        }
   }));
 
 
   //This route updates a course only if the current user owns it.
   router.delete('/courses/:id', authenticateUser, asyncHandler(async(req,res) => {
-    const course = await Course.findOne({id: req.params.id});
-    const user = await User.findByPk(req.currentUser.id);
-
-    if (course && user) {
-        if (req.currentUser.id === req.params.userId) {
+    const course = await Course.findByPk(req.params.id);
+    
+    if (course) {
+        if (course.userId === req.currentUser.id) {
             Course.destroy({   where: {id: req.params.id} });  
             res.status(204).json({message: 'Course deleted.'}).end();
         } else {
@@ -154,6 +138,6 @@ router.get('/users', authenticateUser, asyncHandler(async(req,res) => {
         res.status(404).json({message: "The course that you are trying to delete does not exist."});
         }
   }));
-  
+
 
   module.exports = router;
